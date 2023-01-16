f03309ee952a3ea43abc5f5abfbe7a491728110c
yanjun <ijoy_010@qq.com>
2023-01-16 23:22:30 +0800

    增加打包地址

--- tjrhttpapi/src/com/bitcnew/http/TjrBaseApi.java
+++ tjrhttpapi/src/com/bitcnew/http/TjrBaseApi.java
@@ -15,16 +15,21 @@ public class TjrBaseApi {
 //    public static String  gamePredictHttp="http://predict.piglobalexchanges.com";
 //    public static String  mApiCropymeBaseUploadFile="http://upload.piglobalexchanges.com";
 //    public static String  mApiWebUri = "http://coupon.piglobalexchanges.com";
+
+
+//    private static final String BASE_URL = "encryptedex.com";
+    private static final String BASE_URL = "worldcoinservice.com";
     //测试环境
-    public static String  stockHomeUri ="market.worldcoinservice.com";
-    public static String  stockHomeUriHttp="http://market.worldcoinservice.com";//retrofit的baseurl必须以"/"结束否则报错
-    public static String  mApiSubPushUrl="api.worldcoinservice.com";
-//    public static String  mApiCropymeBaseUri="http://api.worldcoinservice.com";
-    public static String  mApiCropymeBaseUri="http://api.worldcoinservice.com";
-    public static String  gamePredictSocket="predict.worldcoinservice.com";
-    public static String  gamePredictHttp="http://predict.worldcoinservice.com";
-    public static String  mApiCropymeBaseUploadFile="http://upload.worldcoinservice.com";
-    public static String  mApiWebUri = "http://coupon.worldcoinservice.com";
+    public static String  stockHomeUri = "market." + BASE_URL;
+    public static String  stockHomeUriHttp = "http://market." + BASE_URL;//retrofit的baseurl必须以"/"结束否则报错
+    public static String  mApiSubPushUrl = "api." + BASE_URL;
+//    public static String  mApiCropymeBaseUri = "http://api." + BASE_URL;
+    public static String  mApiCropymeBaseUri = "http://api." + BASE_URL;
+    public static String  gamePredictSocket = "predict." + BASE_URL;
+    public static String  gamePredictHttp = "http://predict." + BASE_URL;
+    public static String  mApiCropymeBaseUploadFile = "http://upload." + BASE_URL;
+    public static String  mApiWebUri = "http://coupon." + BASE_URL;
+
 
     // 出外网一定要修改成false
     public static final boolean isDebug;
